d95d9b122a64eff6b1608747237535fe74dd5d15
bw003 <116071092+bw003@users.noreply.github.com>
2024-01-13 12:35:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -182,7 +182,7 @@ if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 if youtube_t=$(toml_get_table "YouTube"); then youtube_mode=$(toml_get "$youtube_t" "build-mode") || youtube_mode="apk"; else youtube_mode="module"; fi
 if music_t=$(toml_get_table "Music"); then music_mode=$(toml_get "$music_t" "build-mode") || music_mode="apk"; else music_mode="module"; fi
 if [ "$youtube_mode" != module ] || [ "$music_mode" != module ]; then
-	log "\nInstall [mMicrog](https://github.com/kazimmt/mMicroG/releases) for non-root YouTube and YT Music"
+	log "\nInstall [microg](https://github.com/inotia00/VancedMicroG/releases/) for non-root YouTube and YT Music"
 fi
 if [ "$youtube_mode" != apk ] || [ "$music_mode" != apk ]; then
 	log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) module to detach YouTube and YT Music from Play Store"
